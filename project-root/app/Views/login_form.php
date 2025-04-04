b9264c0c7e7b0868b5296f500d29e7de6c8ec0df
Cesar Ramirez <ramirezcesar017@gmail.com>
2025-04-04 17:27:03 +0200

    Header et Footer OK

--- project-root/app/Views/login_form.php
+++ project-root/app/Views/login_form.php
@@ -1,21 +1,19 @@
-<DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
+<?php require_once("template/head.php"); ?>
 
 <body>
-    <h1>Se connecter</h1>
-    <form method="POST" action="/login">
-        <label for="login">matricule abonné / identifiant admin</label>
-        <input id="login" name="login" type="text" />
-        <label for="password">nom abonné / mot de passe admin </label>
-        <input id="password" name="password" type="password" />
-        <button type="submit">Se connecter</button>
-    </form>
-</body>
+    <div class="container">
+        <h1>Se connecter</h1>
+        <form method="POST" action="/login">
+            <label for="login">Matricule abonné / Identifiant admin</label>
+            <input id="login" name="login" type="text" />
 
+            <label for="password">Nom abonné / Mot de passe admin</label>
+            <input id="password" name="password" type="password" />
+
+            <button type="submit">Se connecter</button>
+        </form>
+    </div>
+</body>
 </html>
+
+<?php require_once("template/footer.php"); ?>
